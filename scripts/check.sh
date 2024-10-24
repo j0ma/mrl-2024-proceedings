f35a1aea9561190a5c6de708b6aef3bb93af8a01
Jonne Sälevä <jonne@jonnesaleva.com>
2024-10-24 16:55:54 -0400

    Modify script so it works properly now

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,6 +2,8 @@
 
 cd ./proceedings/papers
 
+root=$(pwd)
+
 export paper_id=${1}
 export input_file=00-todo-check/${paper_id}.pdf
 export paper_type=${2:-long}
@@ -14,15 +16,15 @@ cd ${staging_folder}
 
 aclpubcheck -p ${paper_type} ${paper_id}.pdf
 
-cd ..
-
-if [ "$(cat errors-1-no-offending-pages.json)" = "{}" ]
+if [ "$(cat errors-${paper_id}.json)" = "{}" ]
 then
     export destination_folder=02-clean
 else
-    export destination_folder=03-nonerrors
+    export destination_folder=03-nonclean
 fi
 
-cp -r ${staging_folder} ${destination_folder}/${paper_id}
+cd ${root}
+
+mv ${staging_folder} ${destination_folder}/${paper_id}
 
 echo "Check completed for ${paper_id}. Results are in ${destination_folder}/${paper_id}"
